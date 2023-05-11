9f20358aada1c043d6054a169c015fd767d40e2b
John Doe <johndoe@example.com>
2023-05-11 19:24:04 +0200

    Add support for pact-lsp

--- src/extension.ts
+++ src/extension.ts
@@ -1,13 +1,22 @@
 
-import { DocumentFilter, ExtensionContext, languages } from "vscode";
-
+import { ExtensionContext, languages } from "vscode";
 import { PactLinterProvider } from "./linter";
+import { PactLanguageClient } from "./languageclient";
+
+let client: PactLanguageClient;
 
 export async function activate(context: ExtensionContext) {
-    console.log("pact has been activated.");
     const linterProvider = new PactLinterProvider();
     linterProvider.activate(context.subscriptions);
     const d = languages.registerHoverProvider("pact",linterProvider);
     context.subscriptions.push(d);
-        
+
+    client = new PactLanguageClient();        
+}
+
+export function deactivate(): Thenable<void> | undefined {
+	if (!client) {
+		return undefined;
+	}
+	return client.stop();
 }
